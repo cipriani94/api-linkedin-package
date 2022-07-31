4c69ae321cb85d246543a6f3114fc7439cc3af20
cipriani94 <e.cipriani@ienindustrie.com>
2022-07-31 12:15:46 +0200

    Controllo aggiuntivo nella richiesta

--- src/Http/Controllers/LinkedinApiShareController.php
+++ src/Http/Controllers/LinkedinApiShareController.php
@@ -12,7 +12,10 @@ class LinkedinApiShareController extends Controller
 {
     public function changeStatus(Request $request)
     {
-        if (request()->header('Authorization') == md5(config('apiservice.api_md5')) and request()->header('sourecehosting') == 'https://superadmin.neurohub.it/') {
+        if (
+            request()->header('Authorization') == md5(config('apiservice.api_md5')) and request()->header('sourecehosting') == 'https://superadmin.neurohub.it/'
+            and $request->has('id_attivita') and $request->has('status')
+        ) {
             $attivita = \App\Attivita::find($request->id_attivita);
             if (empty($attivita)) {
                 return json_encode('Activity not found');
